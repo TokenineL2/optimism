b55c3e92a59b5eb22c1b07b928cfd029ee76c30b
clabby <ben@clab.by>
2022-11-02 02:14:34 -0400

    Start merkle trie input fuzzer

--- packages/contracts-bedrock/contracts/test/MerkleTrie.t.sol
+++ packages/contracts-bedrock/contracts/test/MerkleTrie.t.sol
@@ -4,7 +4,19 @@ pragma solidity 0.8.15;
 import { CommonTest } from "./CommonTest.t.sol";
 import { MerkleTrie } from "../libraries/trie/MerkleTrie.sol";
 
+struct TrieTestCase {
+    uint256 root;
+    uint256 key;
+    bytes[] proof;
+}
+
+interface NewVm {
+    function parseJson(string memory json) external pure returns (bytes memory);
+}
+
 contract MerkleTrie_Test is CommonTest {
+    function setUp() external {}
+
     function test_get_validProofSucceeds1() external {
         bytes32 root = 0xd582f99275e227a1cf4284899e5ff06ee56da8859be71b553397c69151bc942f;
         bytes memory key = hex"6b6579326262";
@@ -14,50 +26,31 @@ contract MerkleTrie_Test is CommonTest {
         proof[1] = hex"f84580a0582eed8dd051b823d13f8648cdcd08aa2d8dac239f458863c4620e8c4d605debca83206262856176616c32ca83206363856176616c3380808080808080808080808080";
         proof[2] = hex"ca83206262856176616c32";
 
-        assertEq(
-            val,
-            MerkleTrie.get(
-                key,
-                proof,
-                root
-            )
-        );
+        assertEq(val, MerkleTrie.get(key, proof, root));
     }
 
     function test_get_validProofSucceeds2() external {
         bytes32 root = 0xd582f99275e227a1cf4284899e5ff06ee56da8859be71b553397c69151bc942f;
         bytes memory key = hex"6b6579316161";
-        bytes memory val = hex"303132333435363738393031323334353637383930313233343536373839303132333435363738397878";
+        bytes
+            memory val = hex"303132333435363738393031323334353637383930313233343536373839303132333435363738397878";
         bytes[] memory proof = new bytes[](3);
         proof[0] = hex"e68416b65793a03101b4447781f1e6c51ce76c709274fc80bd064f3a58ff981b6015348a826386";
         proof[1] = hex"f84580a0582eed8dd051b823d13f8648cdcd08aa2d8dac239f458863c4620e8c4d605debca83206262856176616c32ca83206363856176616c3380808080808080808080808080";
         proof[2] = hex"ef83206161aa303132333435363738393031323334353637383930313233343536373839303132333435363738397878";
 
-        assertEq(
-            val,
-            MerkleTrie.get(
-                key,
-                proof,
-                root
-            )
-        );
+        assertEq(val, MerkleTrie.get(key, proof, root));
     }
 
     function test_get_validProofSucceeds3() external {
         bytes32 root = 0xf838216fa749aefa91e0b672a9c06d3e6e983f913d7107b5dab4af60b5f5abed;
         bytes memory key = hex"6b6579316161";
-        bytes memory val = hex"303132333435363738393031323334353637383930313233343536373839303132333435363738397878";
+        bytes
+            memory val = hex"303132333435363738393031323334353637383930313233343536373839303132333435363738397878";
         bytes[] memory proof = new bytes[](1);
         proof[0] = hex"f387206b6579316161aa303132333435363738393031323334353637383930313233343536373839303132333435363738397878";
 
-        assertEq(
-            val,
-            MerkleTrie.get(
-                key,
-                proof,
-                root
-            )
-        );
+        assertEq(val, MerkleTrie.get(key, proof, root));
     }
 
     function test_get_validProofSucceeds4() external {
@@ -67,33 +60,20 @@ contract MerkleTrie_Test is CommonTest {
         bytes[] memory proof = new bytes[](1);
         proof[0] = hex"ce87206b6579316161853031323334";
 
-        assertEq(
-            val,
-            MerkleTrie.get(
-                key,
-                proof,
-                root
-            )
-        );
+        assertEq(val, MerkleTrie.get(key, proof, root));
     }
 
     function test_get_validProofSucceeds5() external {
         bytes32 root = 0xcb65032e2f76c48b82b5c24b3db8f670ce73982869d38cd39a624f23d62a9e89;
         bytes memory key = hex"6b657931";
-        bytes memory val = hex"30313233343536373839303132333435363738393031323334353637383930313233343536373839566572795f4c6f6e67";
+        bytes
+            memory val = hex"30313233343536373839303132333435363738393031323334353637383930313233343536373839566572795f4c6f6e67";
         bytes[] memory proof = new bytes[](3);
         proof[0] = hex"e68416b65793a0f3f387240403976788281c0a6ee5b3fc08360d276039d635bb824ea7e6fed779";
         proof[1] = hex"f87180a034d14ccc7685aa2beb64f78b11ee2a335eae82047ef97c79b7dda7f0732b9f4ca05fb052b64e23d177131d9f32e9c5b942209eb7229e9a07c99a5d93245f53af18a09a137197a43a880648d5887cce656a5e6bbbe5e44ecb4f264395ccaddbe1acca80808080808080808080808080";
         proof[2] = hex"f862808080808080a057895fdbd71e2c67c2f9274a56811ff5cf458720a7fa713a135e3890f8cafcf8808080808080808080b130313233343536373839303132333435363738393031323334353637383930313233343536373839566572795f4c6f6e67";
 
-        assertEq(
-            val,
-            MerkleTrie.get(
-                key,
-                proof,
-                root
-            )
-        );
+        assertEq(val, MerkleTrie.get(key, proof, root));
     }
 
     function test_get_validProofSucceeds6() external {
@@ -105,14 +85,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[1] = hex"f87180a034d14ccc7685aa2beb64f78b11ee2a335eae82047ef97c79b7dda7f0732b9f4ca05fb052b64e23d177131d9f32e9c5b942209eb7229e9a07c99a5d93245f53af18a09a137197a43a880648d5887cce656a5e6bbbe5e44ecb4f264395ccaddbe1acca80808080808080808080808080";
         proof[2] = hex"df808080808080c9823262856176616c338080808080808080808573686f7274";
 
-        assertEq(
-            val,
-            MerkleTrie.get(
-                key,
-                proof,
-                root
-            )
-        );
+        assertEq(val, MerkleTrie.get(key, proof, root));
     }
 
     function test_get_validProofSucceeds7() external {
@@ -124,14 +97,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[1] = hex"f87180a034d14ccc7685aa2beb64f78b11ee2a335eae82047ef97c79b7dda7f0732b9f4ca05fb052b64e23d177131d9f32e9c5b942209eb7229e9a07c99a5d93245f53af18a09a137197a43a880648d5887cce656a5e6bbbe5e44ecb4f264395ccaddbe1acca80808080808080808080808080";
         proof[2] = hex"f839808080808080c9823363856176616c338080808080808080809f31323334353637383930313233343536373839303132333435363738393031";
 
-        assertEq(
-            val,
-            MerkleTrie.get(
-                key,
-                proof,
-                root
-            )
-        );
+        assertEq(val, MerkleTrie.get(key, proof, root));
     }
 
     function test_get_validProofSucceeds8() external {
@@ -143,14 +109,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[1] = hex"d780c22061c22062c2206380808080808080808080808080";
         proof[2] = hex"c22061";
 
-        assertEq(
-            val,
-            MerkleTrie.get(
-                key,
-                proof,
-                root
-            )
-        );
+        assertEq(val, MerkleTrie.get(key, proof, root));
     }
 
     function test_get_validProofSucceeds9() external {
@@ -162,14 +121,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[1] = hex"d780c22061c22062c2206380808080808080808080808080";
         proof[2] = hex"c22062";
 
-        assertEq(
-            val,
-            MerkleTrie.get(
-                key,
-                proof,
-                root
-            )
-        );
+        assertEq(val, MerkleTrie.get(key, proof, root));
     }
 
     function test_get_validProofSucceeds10() external {
@@ -181,14 +133,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[1] = hex"d780c22061c22062c2206380808080808080808080808080";
         proof[2] = hex"c22063";
 
-        assertEq(
-            val,
-            MerkleTrie.get(
-                key,
-                proof,
-                root
-            )
-        );
+        assertEq(val, MerkleTrie.get(key, proof, root));
     }
 
     function test_get_reverts_nonexistentKey1() external {
@@ -200,11 +145,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[2] = hex"ca83206262856176616c32";
 
         vm.expectRevert("MerkleTrie: path remainder must share all nibbles with key");
-        MerkleTrie.get(
-            key,
-            proof,
-            root
-        );
+        MerkleTrie.get(key, proof, root);
     }
 
     function test_get_reverts_nonexistentKey2() external {
@@ -214,11 +155,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[0] = hex"e68416b65793a03101b4447781f1e6c51ce76c709274fc80bd064f3a58ff981b6015348a826386";
 
         vm.expectRevert("MerkleTrie: path remainder must share all nibbles with key");
-        MerkleTrie.get(
-            key,
-            proof,
-            root
-        );
+        MerkleTrie.get(key, proof, root);
     }
 
     function test_get_reverts_wrongKeyProof() external {
@@ -230,11 +167,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[2] = hex"d687206e6f746865728d33343938683472697568677765";
 
         vm.expectRevert("MerkleTrie: invalid internal node hash");
-        MerkleTrie.get(
-            key,
-            proof,
-            root
-        );
+        MerkleTrie.get(key, proof, root);
     }
 
     function test_get_reverts_corruptedProof() external {
@@ -248,11 +181,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[4] = hex"ca83206262856176616c32";
 
         vm.expectRevert("RLPReader: decoded item type for list is not a list item");
-        MerkleTrie.get(
-            key,
-            proof,
-            root
-        );
+        MerkleTrie.get(key, proof, root);
     }
 
     function test_get_reverts_invalidDataRemainder() external {
@@ -264,11 +193,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[2] = hex"c32081aa000000000000000000000000000000";
 
         vm.expectRevert("RLPReader: list item has an invalid data remainder");
-        MerkleTrie.get(
-            key,
-            proof,
-            root
-        );
+        MerkleTrie.get(key, proof, root);
     }
 
     function test_get_reverts_invalidInternalNodeHash() external {
@@ -280,11 +205,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[2] = hex"de2a9c6a46b6ea71ab9e881c8420570cf19e833c85df6026b04f085016e78f";
 
         vm.expectRevert("MerkleTrie: invalid internal node hash");
-        MerkleTrie.get(
-            key,
-            proof,
-            root
-        );
+        MerkleTrie.get(key, proof, root);
     }
 
     function test_get_reverts_zeroBranchValueLength() external {
@@ -295,11 +216,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[1] = hex"d98080808080808080808080c43b82aabbc43c82aacc80808080";
 
         vm.expectRevert("MerkleTrie: value length must be greater than zero (branch)");
-        MerkleTrie.get(
-            key,
-            proof,
-            root
-        );
+        MerkleTrie.get(key, proof, root);
     }
 
     function test_get_reverts_zeroLengthKey() external {
@@ -309,11 +226,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[0] = hex"c78320f00082b443";
 
         vm.expectRevert("MerkleTrie: path remainder must share all nibbles with key");
-        MerkleTrie.get(
-            key,
-            proof,
-            root
-        );
+        MerkleTrie.get(key, proof, root);
     }
 
     function test_get_reverts_smallerPathThanKey1() external {
@@ -325,11 +238,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[2] = hex"c582202381aa";
 
         vm.expectRevert("MerkleTrie: path remainder must share all nibbles with key");
-        MerkleTrie.get(
-            key,
-            proof,
-            root
-        );
+        MerkleTrie.get(key, proof, root);
     }
 
     function test_get_reverts_smallerPathThanKey2() external {
@@ -341,11 +250,7 @@ contract MerkleTrie_Test is CommonTest {
         proof[2] = hex"e48200bba0a6911545ed01c2d3f4e15b8b27c7bfba97738bd5e6dd674dd07033428a4c53af";
 
         vm.expectRevert("MerkleTrie: path remainder must share all nibbles with key");
-        MerkleTrie.get(
-            key,
-            proof,
-            root
-        );
+        MerkleTrie.get(key, proof, root);
     }
 
     function test_get_reverts_extraProofElements() external {
@@ -358,10 +263,23 @@ contract MerkleTrie_Test is CommonTest {
         proof[3] = hex"c32081aa";
 
         vm.expectRevert("MerkleTrie: value node must be last node in proof (leaf)");
-        MerkleTrie.get(
-            key,
-            proof,
-            root
-        );
+        MerkleTrie.get(key, proof, root);
+    }
+
+    function testFuzzTrie_validProofs() external {
+        for (uint256 i = 0; i < 1024; ++i) {
+            // TODO: Make sure that the `merkle-trie-fuzzer` is built before running
+            //       the CI tests.
+            string[] memory cmds = new string[](1);
+            cmds[0] = "./scripts/merkle-trie-fuzzer/target/release/patricia";
+            bytes memory res = vm.ffi(cmds);
+            (bytes32 root, bytes memory key, bytes memory val, bytes[] memory proof) = abi.decode(
+                res,
+                (bytes32, bytes, bytes, bytes[])
+            );
+
+            // Assert that our expected value is equal to our actual value.
+            assertEq(val, MerkleTrie.get(key, proof, root));
+        }
     }
 }
